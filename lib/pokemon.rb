92d6e3c21b75bfdaa9dafaad4393ced35f80e0ca
Luis Martinez <luis_mmartinez@live.com>
2021-03-11 23:11:35 -0500

    commit tree d5bdf08a59ac51d6e7cf509f059598d900eef273

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,21 +1,25 @@
+
+require 'pry'
+
 class Pokemon
-    attr_accessor :id, :name, :type, :db
+  attr_accessor :name, :type, :db
+  attr_reader :id
+  
+  def initialize(id: nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db= db
+  end
 
-    def initialize (id:, name:, type:, db:)
-        @id = id
-        @name = name
-        @type = type
-        @db = db
-    end
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
 
-    def self.save(name, type, db)
-        db.execute( "INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
-    end
-
-    def self.find(id, db)
-        info= db.execute("SELECT * FROM pokemon WHERE id= ?", id).flatten
-        self.new(id:info[0], name:info[1], type:info[2], db:db)
-    end
-
+  def self.find(id, db)
+    info= db.execute("SELECT * FROM pokemon WHERE id= ?", id).flatten
+    binding.pry
+    pokemon = self.new(id:info[0], name:info[1], type:info[2], db:db)
+  end
 
 end
